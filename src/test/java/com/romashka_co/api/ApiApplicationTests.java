eb309b80da78891f07c6cf4ff0e5b3c62f9ffe31
cybersobic <cybersobic@gmail.com>
2024-11-20 08:16:08 +0300

    Инициализация проекта

---
+++ src/test/java/com/romashka_co/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romashka_co.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
