5b437a4779ca62fa43f9e9d25b24cfa0d5d98ebb
cybersobic <cybersobic@gmail.com>
2024-11-21 12:28:36 +0300

    Стартовое подключение СУБД PostgreSQL

--- src/main/java/com/romashka_co/api/services/ProductService.java
+++ src/main/java/com/romashka_co/api/services/ProductService.java
@@ -4,7 +4,6 @@ import com.romashka_co.api.exceptions.ProductCreationException;
 import com.romashka_co.api.exceptions.ProductNotFoundException;
 import com.romashka_co.api.models.Product;
 import com.romashka_co.api.repos.ProductRepository;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
